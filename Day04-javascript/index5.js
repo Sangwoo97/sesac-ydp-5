1aab76929e49f622d8984e3373f13f98aa5bad3f
Sangwoo97 <aa6618@naver.com>
2023-07-28 14:02:48 +0900

    study: javascript마무리

--- Day04-javascript/index5.js
+++ Day04-javascript/index5.js
@@ -1,78 +1,78 @@
-// // 조건문
+// 조건문
 
-// // if문
-// if (5 > 3) {
-//     console.log('얍!');
-// }
+// if문
+if (5 > 3) {
+    console.log('얍!');
+}
 
-// let number = Number(prompt('숫자를 입력해주세요!'));
-// // 1. prompt() 로 사용자로부터 값을 입력받고(문자열)
-// // 2. Number() 문자열-> 숫자형 형변환
-// // 아래와 동일한 코드
-// // let number2 = prompt('숫자 입력해주세요!')
-// // number2 = Number(number2)
+let number = Number(prompt('숫자를 입력해주세요!'));
+// 1. prompt() 로 사용자로부터 값을 입력받고(문자열)
+// 2. Number() 문자열-> 숫자형 형변환
+// 아래와 동일한 코드
+// let number2 = prompt('숫자 입력해주세요!')
+// number2 = Number(number2)
 
-// // if (number > 10) {
-// //   console.log('입력한 수는 10보다 크군요!');
-// // } else {
-// //   console.log('입력한 수는 10보다 작거나 같네요!');
-// // }
-
-// // if (number > 10) {
-// //   console.log('입력한 수는 10보다 크군요!');
-// // } else if (number === 10) {
-// //   console.log('입력한 수는 10이네요!');
-// // } else {
-// //   console.log('입력한 수는 10보다 작네요!');
-// // }
-
-// if (number > 100 || number < 0) {
-//     console.log('입력값이 잘못되었습니다. 숫자의 범위는 0 ~ 100');
-// } else if (number >= 90) {
-//     console.log('A');
-// } else if (number >= 80) {
-//     console.log('B');
-// } else if (number >= 70) {
-//     console.log('C');
-// } else if (number >= 60) {
-//     console.log('D');
+// if (number > 10) {
+//   console.log('입력한 수는 10보다 크군요!');
 // } else {
-//     console.log('F');
+//   console.log('입력한 수는 10보다 작거나 같네요!');
 // }
 
-// // 중첩 if문
-// let userId = 'user01';
-// let userPw = '1234';
-
-// // id, pw 검사하는 함수
-// function loginUser() {
-//     let inputId = prompt('아이디 입력');
-//     let inputPw = prompt('비밀번호 입력');
-
-//     // - userId와 inputId가 같다면
-//     //    - userPw와 inputPw 를 비교
-//     // - inputId에 빈값 입력됐다면
-//     //    - '아이디 입력 안함' 문구 반환
-//     // - 두 경우가 아니라면 (inputId가 틀렸을 때)
-//     //    - '아이디 틀림' 문구 반환
-
-//     if (userId === inputId) {
-//         if (userPw === inputPw) {
-//             return '로그인 성공';
-//         } else {
-//             return '비번 오류! 로그인 실패';
-//         }
-//     } else if (inputId === '') {
-//         return '아이디 입력 안했음';
-//     } else {
-//         return '아이디 오류! 로그인 실패';
-//     }
+// if (number > 10) {
+//   console.log('입력한 수는 10보다 크군요!');
+// } else if (number === 10) {
+//   console.log('입력한 수는 10이네요!');
+// } else {
+//   console.log('입력한 수는 10보다 작네요!');
 // }
-// const result = loginUser();
-// // -> loginUser 함수의 리턴값(반환값)을 result 변수에 저장
-// console.log(result);
 
-// console.log(new Date().getHours() > 12 ? '오후' : '오전');
+if (number > 100 || number < 0) {
+    console.log('입력값이 잘못되었습니다. 숫자의 범위는 0 ~ 100');
+} else if (number >= 90) {
+    console.log('A');
+} else if (number >= 80) {
+    console.log('B');
+} else if (number >= 70) {
+    console.log('C');
+} else if (number >= 60) {
+    console.log('D');
+} else {
+    console.log('F');
+}
+
+// 중첩 if문
+let userId = 'user01';
+let userPw = '1234';
+
+// id, pw 검사하는 함수
+function loginUser() {
+    let inputId = prompt('아이디 입력');
+    let inputPw = prompt('비밀번호 입력');
+
+    // - userId와 inputId가 같다면
+    //    - userPw와 inputPw 를 비교
+    // - inputId에 빈값 입력됐다면
+    //    - '아이디 입력 안함' 문구 반환
+    // - 두 경우가 아니라면 (inputId가 틀렸을 때)
+    //    - '아이디 틀림' 문구 반환
+
+    if (userId === inputId) {
+        if (userPw === inputPw) {
+            return '로그인 성공';
+        } else {
+            return '비번 오류! 로그인 실패';
+        }
+    } else if (inputId === '') {
+        return '아이디 입력 안했음';
+    } else {
+        return '아이디 오류! 로그인 실패';
+    }
+}
+const result = loginUser();
+// -> loginUser 함수의 리턴값(반환값)을 result 변수에 저장
+console.log(result);
+
+console.log(new Date().getHours() > 12 ? '오후' : '오전');
 let sum = 0;
 for (let i = 0; i < 101; i++) {
     if (i % 2 === 0 || i % 5 === 0) {
@@ -81,7 +81,7 @@ for (let i = 0; i < 101; i++) {
 }
 console.log(sum);
 
-for (let i = 0; i < 101; i++) {
+for (let i = 0; i < 10001; i++) {
     if (i % 13 === 0 && i % 2 === 1) {
         console.log(i);
     }
@@ -184,3 +184,56 @@ console.log(arr2.join('-'));
 console.log('hello'.split(''));
 console.log('hello'.split('').reverse());
 console.log('hello'.split('').reverse().join(''));
+
+/////////////////////////////////////////////////
+// 배열에서 반복문 사용하기!
+// - 기본 for 문
+// - for of 문
+// - forEach() 메서드
+
+const arr3 = [1, 2, 5, 6, 7];
+const alphabets = ['a', 'b', 'c', 'd'];
+
+for (let a = 0; a < arr3.length; a++) {
+    console.log(arr3[a]);
+}
+
+for (let alpha of alphabets) {
+    console.log(alpha);
+}
+
+alphabets.forEach(function (alpha) {
+    // alpha: currentValue를 의미. 반복하고 있는 현재 요소
+    console.log(alpha);
+});
+
+alphabets.forEach(function (alpha, idx) {
+    // alpha: currentValue를 의미. 반복하고 있는 현재 요소
+    // idx: currentValue의 인덱스(위치)
+    console.log(alpha, idx);
+});
+
+alphabets.forEach(function (alpha, idx, arr) {
+    // alpha: currentValue를 의미. 반복하고 있는 현재 요소
+    // idx: currentValue의 인덱스(위치)
+    // arr: forEach를 호출한 배열
+    console.log(alpha, idx, arr);
+});
+
+// map, filter, find 메서드
+const arr4 = [1, 2, 3, 4, 5];
+
+// map(): 배열 내 모든 원소에 대해 연산한 결과를 모아 "새로운 배열" 반환
+// const mapResult = arr4.map(function (e) {
+//   return e * 2;
+// });
+const mapResult = arr4.map((e) => e * 2);
+console.log(mapResult);
+
+// filter(): 주어진 함수의 테스트(조건)를 통과하는 요소를 모아
+// (조건 통과하면 요소 유지, 미통과하면 요소 버림) "새로운 배열" 반환
+// const filterResult = arr4.filter(function (e) {
+//   return e > 2;
+// });
+const filterResult = arr4.filter((e) => e > 2);
+console.log(filterResult);
