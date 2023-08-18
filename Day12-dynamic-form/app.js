a76b6a85d21d217fe1f985fa09e1dfb5f3cdb746
Sangwoo97 <aa6618@naver.com>
2023-08-18 18:45:27 +0900

    study: fetch, axios, ajax 공부 및 실습

--- Day12-dynamic-form/app.js
+++ Day12-dynamic-form/app.js
@@ -13,13 +13,41 @@ app.listen(PORT, () => {
 });
 
 app.get('/', (req, res) => {
-    res.render('dynamic');
+    res.render('practice2');
 });
 
 app.get('/ajax', (req, res) => {
     res.send(req.query);
 });
 
+app.get('/axios', (req, res) => {
+    res.send(req.query);
+});
+
 app.post('/ajax', (req, res) => {
     res.send(req.body);
+});
+
+app.post('/axios', (req, res) => {
+    res.send(req.body);
+});
+
+app.get('/fetch', (req, res) => {
+    res.send(req.query);
+});
+
+app.post('/fetch', (req, res) => {
+    res.send(req.body);
+});
+
+app.post('/prac', (req, res) => {
+    const id = 'sesac';
+    const pw = '1234';
+    if (req.body.id !== id) {
+        res.send({ error: '아이디가 올바르지 않습니다.' });
+    } else if (req.body.pw !== pw) {
+        res.send({ error: '비밀번호가 올바르지 않습니다.' });
+    } else {
+        res.send({ result: '로그인 성공!' });
+    }
 });
