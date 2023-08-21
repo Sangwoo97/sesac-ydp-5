4051db44d37afc4561faf8beb8bc012fbcbca0d5
Sangwoo97 <aa6618@naver.com>
2023-08-21 15:32:12 +0900

    study: multer 세부설정

--- Day13-file-upload/app.js
+++ Day13-file-upload/app.js
@@ -3,6 +3,19 @@ const app = express();
 const PORT = 8000;
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
+const path = require('path');
+const uploadDetail = multer({
+    storage: multer.diskStorage({
+        destination(req, file, done) {
+            done(null, 'uploads/');
+        },
+        filename(req, file, done) {
+            const ext = path.extname(file.originalname); // 파일 '확장자' 추출
+            done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+        },
+    }),
+    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+});
 
 app.set('view engine', 'ejs');
 
@@ -18,7 +31,7 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.post('/upload', upload.single('userFile'), (req, res) => {
+app.post('/upload', uploadDetail.single('userFile'), (req, res) => {
     console.log(req.file);
     console.log(req.body);
     res.send('파일 업로드 완료!');
