0ca1d47ff7bea4de7468c3452c903eff0d7b7019
Sangwoo97 <aa6618@naver.com>
2023-09-01 17:13:11 +0900

    study: sequelize association

--- Day17_sequelize_upgrade_release/controller/Cmain.js
+++ Day17_sequelize_upgrade_release/controller/Cmain.js
@@ -1,1 +1,118 @@
+const { Player, Profile, Team } = require('../models');
+const { Op } = require('sequelize');
+
 // TODO: 컨트롤러
+
+exports.index = (req, res) => {
+    res.render('index');
+};
+
+exports.getPlayers = async (req, res) => {
+    try {
+        const players = await Player.findAll();
+        res.send(players);
+    } catch (err) {
+        console.error(err);
+        res.send('Internal Server Error!!!');
+    }
+};
+
+exports.getPlayer = async (req, res) => {
+    try {
+        const { player_id } = req.params;
+        const player = await Player.findOne({
+            where: { player_id: player_id },
+        });
+        res.send(player);
+    } catch (err) {
+        console.error(err);
+        res.send('Internal Server Error!!!');
+    }
+};
+
+exports.postPlayer = async (req, res) => {
+    try {
+        const { name, age, team_id } = req.body;
+        const newPlayer = await Player.create({
+            name: name,
+            age: age,
+            team_id: team_id,
+        });
+        res.send(newPlayer);
+    } catch (err) {
+        console.error(err);
+        res.send('Internal Server Error!!!');
+    }
+};
+
+exports.patchPlayer = async (req, res) => {
+    try {
+        const { team_id } = req.body;
+        const { player_id } = req.params;
+        const updatedPlayer = await Player.update({ team_id: team_id }, { where: { player_id: player_id } });
+        res.send(updatedPlayer);
+    } catch (err) {
+        console.error(err);
+        res.send('Internal Server Error!!!');
+    }
+};
+
+exports.deletePlayer = async (req, res) => {
+    try {
+        const { player_id } = req.params;
+        const isDeleted = await Player.destroy({ where: { player_id: player_id } });
+        res.send(isDeleted ? true : false);
+    } catch (err) {
+        console.error(err);
+        res.send('Internal Server Error!!!');
+    }
+};
+
+exports.getTeams = async (req, res) => {
+    try {
+        const { sort, search } = req.query;
+        const teams = await Team.findAll({
+            attributes: ['team_id', 'name'],
+            order: sort === 'name_asc' ? [['name', 'asc']] : undefined,
+            where: search
+                ? {
+                      name: { [Op.like]: `%${search}%` },
+                  }
+                : undefined,
+        });
+        res.send(teams);
+    } catch (err) {
+        console.error(err);
+        res.send('Internal Server Error!!!');
+    }
+};
+
+exports.getTeam = async (req, res) => {
+    try {
+        const { team_id } = req.params;
+        const team = await Team.findOne(
+            { attributes: ['team_id', 'name'] },
+            {
+                where: { team_id: team_id },
+            }
+        );
+        res.send(team);
+    } catch (err) {
+        console.error(err);
+        res.send('Internal Server Error!!!');
+    }
+};
+
+exports.getTeamPlayers = async (req, res) => {
+    try {
+        const { team_id } = req.params;
+        const team = await Team.findOne({
+            where: { team_id: team_id },
+            include: [{ model: Player }],
+        });
+        res.send(team);
+    } catch (err) {
+        console.error(err);
+        res.send('Internal Server Error!!!');
+    }
+};
--- Day17_sequelize_upgrade_release/index.sql
+++ Day17_sequelize_upgrade_release/index.sql
@@ -12,7 +12,7 @@ select * from Profile;
 select * from Team;
 
 -- Players table 에 on delete cascade 확인 완료 (Profile table 연쇄 적용)
-delete from Player where plyaer_id = 1;
+delete from Player where player_id = 1;
 
 -- 테이블 삭제 
 -- 테이블간 관계가 있으므로 삭제시 순서 주의
